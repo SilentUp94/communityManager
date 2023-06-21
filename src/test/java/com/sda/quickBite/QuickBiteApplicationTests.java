4aaf4dd40c35dfea39716076d27565f4dd233c08
Costin Ionut <Costin.ionut94@gmail.com>
2023-06-21 19:54:10 +0200

    aaa

---
+++ src/test/java/com/sda/quickBite/QuickBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.quickBite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
